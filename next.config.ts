8ef59b7db7aad73f751d7473193a7da8f6f84518
Devin Daniels <devingdaniels@gmail.com>
2025-01-19 22:18:27 +0530

    fixed home redirect

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"], // Add the domain here
   },
+  async redirects() {
+    return [
+      {
+        source: "/", // The root path
+        destination: "/auth/login", // The desired destination
+        permanent: true, // Indicates whether this is a permanent redirect (status code 308)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
