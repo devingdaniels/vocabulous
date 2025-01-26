f88471ba18afe49382323cb23666169ced5e0386
Devin Daniels <devingdaniels@gmail.com>
2025-01-26 14:59:34 +0530

    linting

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,6 @@
-import { getUserFromLocalStorage } from "@/utils";
 import axios from "axios";
 
 export const getUserInfo = async () => {
-  const { user, token } = getUserFromLocalStorage();
   const url = "https://api.vocabulous.xyz/api/user";
   axios.defaults.withCredentials = true;
   const response = await axios.get(url);
