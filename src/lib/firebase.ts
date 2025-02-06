1fd347edf33b7c5afe509527bbe55031aab7ca1f
kclaka <igbechik@oregonstate.edu>
2025-02-05 22:47:02 -0800

    refactor: Move Firebase config to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVvufRUApANwEZ2XVCdgmo_e7t3X9-zfU",
-  authDomain: "vocabulous-eb427.firebaseapp.com",
-  projectId: "vocabulous-eb427",
-  storageBucket: "vocabulous-eb427.appspot.com",
-  messagingSenderId: "624609192018",
-  appId: "1:624609192018:web:1ca14dbd8196ce219a578b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
